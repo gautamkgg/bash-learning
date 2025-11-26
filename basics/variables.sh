0d5bc2931f14b6c53c5fd2424165021fe0ec3554
Gautam Kumar <gautamkgg2025@gmial.com>
2025-11-26 15:56:48 +0530

    Added basic bash script and notes

---
+++ basics/variables.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+# Local variables
+name="Gautam"
+age=22
+course="cybersecurity"
+
+echo "My name is $name"
+echo "My age is $age"
+echo "My course is $course"
+
+# Global variables
+export country="India"
+export courseof="BashScripting"
+
+echo "My country is $country"
+echo "My course of study is $courseof"
